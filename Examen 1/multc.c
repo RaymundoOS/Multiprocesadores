33c84404a5346e0cde17a1f71c4256bf0c1e51a6
Raymundo Ortiz Salazar <49320379+RaymundoOS@users.noreply.github.com>
2021-09-17 00:13:20 -0500

    Add files via upload

---
+++ Examen 1/multc.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+
+int main(int argc, char const *argv[])
+{
+	int m[600][600];
+	int n[600][600];
+	int rest[600][600];
+	int sum = 0;
+
+
+	for (int i = 0; i < 600; ++i)
+	{
+		for (int j = 0; j < 600; ++j)
+		{
+			m[i][j] = 1;
+			n[i][j] = 1;
+		}
+	}
+
+	int NUM_THREADS; 
+	for(NUM_THREADS=1; NUM_THREADS<100; NUM_THREADS++)
+	{
+		int c, d, k;
+		omp_set_num_threads(NUM_THREADS);
+		//omp_set_num_threads(10);
+		const double start = omp_get_wtime();
+		
+		#pragma omp parallel for schedule(dynamic) private (c, d, k, sum)
+		for (c = 0; c < 600; c++) {
+			for (d = 0; d < 600; d++) {
+
+				for (k = 0; k < 600; k+=3) {
+					sum = sum + m[c][k]*n[k][d];
+				}
+				for (k = 0; k < 600; k+=3) {
+					sum = sum + m[c][k+1]*n[k+1][d];
+				}
+				for (k = 0; k < 600; k+=3) {
+					sum = sum + m[c][k+2]*n[k+2][d];
+				}
+			rest[c][d] = sum;
+			sum = 0;
+			}
+		}
+
+		/*
+		for (int c = 0; c < 600; c++) {
+			for (int d = 0; d < 600; d++)
+				printf("%i  ", rest[c][d]);
+
+		printf("\n");
+		}
+		*/
+
+		const double end = omp_get_wtime();
+		printf("%lf\n", (end - start));
+	}
+	return 0;
+}
